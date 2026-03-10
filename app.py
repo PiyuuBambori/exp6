fc8831bb59b8c4d3384df254c3f6cefc44955ff6
Piyush Bambori <piyubambori@gmail.com>
2026-03-10 20:07:57 +0530

    Update index route to include additional text

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Piyush"
+    return "Piyush 2410108"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000,debug=True )
